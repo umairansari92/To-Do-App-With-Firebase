9cf065d3c29303fb55bf46a65888b83f7f6f9388
umairansari92 <umair.ansari.92@gmail.com>
2025-07-12 21:33:12 +0500

    second commit

--- JS/fireBase.js
+++ JS/fireBase.js
@@ -14,15 +14,7 @@ import {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyB2ysNQLsh16tAlpWokJj9L7oXN5pjipnQ",
-    authDomain: "to-do-app-dc044.firebaseapp.com",
-    projectId: "to-do-app-dc044",
-    storageBucket: "to-do-app-dc044.firebasestorage.app",
-    messagingSenderId: "247900712018",
-    appId: "1:247900712018:web:9ebfc83ed1bc163e6f038b",
-    measurementId: "G-9JENPEXLDZ"
-};
+import { firebaseConfig } from "./config.js"
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
